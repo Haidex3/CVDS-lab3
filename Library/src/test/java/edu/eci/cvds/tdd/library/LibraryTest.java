b3e7e29f7293ab253289d187691ca01cd9e760f0
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2024-08-31 12:43:02 -0500

    Update LibraryTest.java

--- Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -1,35 +1,21 @@
-package domain;
+package edu.eci.cvds.tdd;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-public class F_SearchTest {
+public class LibraryTest {
     private F_Search fSearch;
     private Garden garden;
     
     @Before
     public void setUp() {
-        garden = new Garden(); 
-        fSearch = new F_Search(garden, 2, 2); 
+        library=new Library();
     }
 
     @Test
     public void testAct() {
-        fSearch.act();
-        assertEquals(1, fSearch.getTime()); 
+        book1=new Book("Libro muy interesante","Desconocido","123");
     }
-    
-    @Test
-    public void testMove() {
-        int initialRow = fSearch.getRow();
-        int initialColumn = fSearch.getColumn();
-        fSearch.act();
-        fSearch.move();
-        
-        int newRow = fSearch.getRow();
-        int newColumn = fSearch.getColumn();
-        assertEquals(initialRow +1, newRow);
-        assertEquals(initialColumn+1, newColumn);
-    }
+
 }
